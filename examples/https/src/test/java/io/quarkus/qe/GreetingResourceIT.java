6e35960a62a2528e7da64df19c12ad15e5ba0149
Jose <josecarvajalhilario@gmail.com>
2021-04-20 10:11:06 +0200

    Fix example on Native

--- examples/https/src/test/java/io/quarkus/qe/GreetingResourceIT.java
+++ examples/https/src/test/java/io/quarkus/qe/GreetingResourceIT.java
@@ -8,12 +8,9 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
-import io.quarkus.test.scenarios.annotations.DisabledOnNative;
 import io.quarkus.test.services.QuarkusApplication;
 
 @QuarkusScenario
-// SSL is not working when running on Native. It needs to be investigated.
-@DisabledOnNative
 public class GreetingResourceIT {
     @QuarkusApplication(ssl = true)
     static final RestService app = new RestService();
