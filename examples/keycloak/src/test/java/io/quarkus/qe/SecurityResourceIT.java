7f571a7cc1dc8a64f7b4ffac7f4312c1ab97a7d5
Jose <josecarvajalhilario@gmail.com>
2021-04-21 12:25:05 +0200

    Added facilities to KeycloakService to create authz clients

--- examples/keycloak/src/test/java/io/quarkus/qe/SecurityResourceIT.java
+++ examples/keycloak/src/test/java/io/quarkus/qe/SecurityResourceIT.java
@@ -2,14 +2,9 @@ package io.quarkus.qe;
 
 import static org.hamcrest.Matchers.equalTo;
 
-import java.util.Collections;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.keycloak.authorization.client.AuthzClient;
-import org.keycloak.authorization.client.Configuration;
 
 import io.quarkus.test.bootstrap.KeycloakService;
 import io.quarkus.test.bootstrap.RestService;
@@ -21,19 +16,16 @@ import io.quarkus.test.services.QuarkusApplication;
 public class SecurityResourceIT {
 
     private static final String REALM_DEFAULT = "test-realm";
-    private static final String CLIENT_ID_DEFAULT = "test-application-client";
-    private static final String CLIENT_SECRET_DEFAULT = "test-application-client-secret";
-    private static final String NORMAL_USER = "test-normal-user";
-
     @Container(image = "quay.io/keycloak/keycloak:11.0.3", expectedLog = "Admin console listening", port = 8080)
     static final KeycloakService keycloak = new KeycloakService("/keycloak-realm.json", REALM_DEFAULT);
-
+    private static final String CLIENT_ID_DEFAULT = "test-application-client";
+    private static final String CLIENT_SECRET_DEFAULT = "test-application-client-secret";
     @QuarkusApplication
     static final RestService app = new RestService()
             .withProperty("quarkus.oidc.auth-server-url", () -> keycloak.getRealmUrl())
             .withProperty("quarkus.oidc.client-id", CLIENT_ID_DEFAULT)
             .withProperty("quarkus.oidc.credentials.secret", CLIENT_SECRET_DEFAULT);
-
+    private static final String NORMAL_USER = "test-normal-user";
     private AuthzClient authzClient;
 
     @BeforeEach
@@ -52,12 +44,7 @@ public class SecurityResourceIT {
     }
 
     private void initAuthzClient() {
-        authzClient = AuthzClient.create(new Configuration(
-                StringUtils.substringBefore(keycloak.getRealmUrl(), "/realms"),
-                REALM_DEFAULT,
-                CLIENT_ID_DEFAULT,
-                Collections.singletonMap("secret", CLIENT_SECRET_DEFAULT),
-                HttpClients.createDefault()));
+        authzClient = keycloak.createAuthzClient(CLIENT_ID_DEFAULT, CLIENT_SECRET_DEFAULT);
     }
 
     private String getTokenByTestNormalUser() {
--- quarkus-test-service-keycloak/src/main/java/io/quarkus/test/bootstrap/KeycloakService.java
+++ quarkus-test-service-keycloak/src/main/java/io/quarkus/test/bootstrap/KeycloakService.java
@@ -1,5 +1,12 @@
 package io.quarkus.test.bootstrap;
 
+import java.util.Collections;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.impl.client.HttpClients;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.Configuration;
+
 public class KeycloakService extends BaseService<KeycloakService> {
 
     private static final String USER = "admin";
@@ -8,13 +15,26 @@ public class KeycloakService extends BaseService<KeycloakService> {
     private final String realm;
 
     public KeycloakService(String file, String realm) {
+        this(realm);
+        withProperty("KEYCLOAK_IMPORT", "resource::" + file);
+    }
+
+    public KeycloakService(String realm) {
         this.realm = realm;
         withProperty("KEYCLOAK_USER", USER);
         withProperty("KEYCLOAK_PASSWORD", PASSWORD);
-        withProperty("KEYCLOAK_IMPORT", "resource::" + file);
     }
 
     public String getRealmUrl() {
         return String.format("%s:%s/auth/realms/%s", getHost(), getPort(), realm);
+    }
+
+    public AuthzClient createAuthzClient(String clientId, String clientSecret) {
+        return AuthzClient.create(new Configuration(
+                StringUtils.substringBefore(getRealmUrl(), "/realms"),
+                realm,
+                clientId,
+                Collections.singletonMap("secret", clientSecret),
+                HttpClients.createDefault()));
     }
 }
