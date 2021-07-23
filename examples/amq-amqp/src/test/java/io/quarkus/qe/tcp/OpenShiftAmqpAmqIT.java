b59ff105dbedcf4123f26872a3af27fb08df9a2d
Jose <josecarvajalhilario@gmail.com>
2021-07-23 08:39:53 +0200

    Disabled amqp amq tests because #quarkus/issues/18956
    Reported by https://github.com/quarkusio/quarkus/issues/18956

--- examples/amq-amqp/src/test/java/io/quarkus/qe/tcp/OpenShiftAmqpAmqIT.java
+++ examples/amq-amqp/src/test/java/io/quarkus/qe/tcp/OpenShiftAmqpAmqIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.qe.tcp;
 
 import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusVersion;
 
+@DisabledOnQuarkusVersion(version = "2\\.1\\..*", reason = "Caused by https://github.com/quarkusio/quarkus/issues/18956")
+@DisabledOnQuarkusVersion(version = "999-SNAPSHOT", reason = "Caused by https://github.com/quarkusio/quarkus/issues/18956")
 @OpenShiftScenario
 public class OpenShiftAmqpAmqIT extends AmqpAmqIT {
 
