690ed1ebc9563e61f92cd2f37561828211bc520d
Jose <josecarvajalhilario@gmail.com>
2021-07-30 11:40:36 +0200

    Fix k8s jobs in CI
    The problem was that the node in Kind was not able to deploy the Metallb (load balancer component) because some missing requirements of the Kind nodes.
    
    The solution was to upgrade Kind to `v0.11.1`.
    I kept the troubleshooting tools that I needed to find out the issue.

--- .github/configure-load-balancer.sh
+++ .github/configure-load-balancer.sh
@@ -2,9 +2,19 @@
 # usage:
 # ./configure-kind-load-balancer.sh <docker_network>
 DOCKER_NETWORK=$1
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/namespace.yaml
+
+#Inspect Kube config (only for troubleshooting)
+kubectl cluster-info
+kubectl get nodes
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/namespace.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/master/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb.yaml
+GET_METALLB_PODS="kubectl get pods -n metallb-system"
+echo "Waiting for the Metallb Controller"
+timeout 240s bash -c "until $GET_METALLB_PODS | grep controller | grep Running; do $GET_METALLB_PODS; kubectl -n metallb-system get events; sleep 10; done"
+echo "Waiting for the Metallb Speaker"
+timeout 240s bash -c "until $GET_METALLB_PODS | grep speaker | grep Running; do $GET_METALLB_PODS; sleep 10; done"
 
 # Inspect docker network (only for troubleshooting)
 docker network inspect -f '{{.IPAM.Config}}' $DOCKER_NETWORK
