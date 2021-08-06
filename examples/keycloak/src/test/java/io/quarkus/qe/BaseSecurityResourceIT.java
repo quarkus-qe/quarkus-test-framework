94e70701973a1da5bc0e49502fffbbdf2edc9c3d
Jose Carvajal <josecarvajalhilario@gmail.com>
2021-08-06 08:59:04 +0200

    Upgrade Keycloak to 14.0.0 (#184)

--- examples/keycloak/src/test/java/io/quarkus/qe/BaseSecurityResourceIT.java
+++ examples/keycloak/src/test/java/io/quarkus/qe/BaseSecurityResourceIT.java
@@ -17,7 +17,7 @@ public abstract class BaseSecurityResourceIT {
     static final String CLIENT_SECRET_DEFAULT = "test-application-client-secret";
     static final String NORMAL_USER = "test-normal-user";
 
-    @Container(image = "quay.io/keycloak/keycloak:11.0.3", expectedLog = "Admin console listening", port = 8080)
+    @Container(image = "quay.io/keycloak/keycloak:14.0.0", expectedLog = "Admin console listening", port = 8080)
     static final KeycloakService keycloak = new KeycloakService("/keycloak-realm.json", REALM_DEFAULT);
 
     private AuthzClient authzClient;
--- examples/keycloak/src/test/java/io/quarkus/qe/OpenShiftUsingCustomTemplateResourceIT.java
+++ examples/keycloak/src/test/java/io/quarkus/qe/OpenShiftUsingCustomTemplateResourceIT.java
@@ -15,7 +15,7 @@ public class OpenShiftUsingCustomTemplateResourceIT {
     private static final String CLIENT_ID_DEFAULT = "test-application-client";
     private static final String CLIENT_SECRET_DEFAULT = "test-application-client-secret";
 
-    @Container(image = "quay.io/keycloak/keycloak:11.0.3", expectedLog = "Admin console listening", port = 8080)
+    @Container(image = "quay.io/keycloak/keycloak:14.0.0", expectedLog = "Admin console listening", port = 8080)
     static final KeycloakService customkeycloak = new KeycloakService("/keycloak-realm.json", REALM_DEFAULT);
 
     @QuarkusApplication
