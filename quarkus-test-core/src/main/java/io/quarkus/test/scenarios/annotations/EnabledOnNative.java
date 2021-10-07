5fb5ed761a6701547dd0fbc195309605869afaa8
Jose <josecarvajalhilario@gmail.com>
2021-10-07 07:01:14 +0200

    Allow annotations to be inherited

--- quarkus-test-core/src/main/java/io/quarkus/test/scenarios/annotations/EnabledOnNative.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/scenarios/annotations/EnabledOnNative.java
@@ -1,12 +1,14 @@
 package io.quarkus.test.scenarios.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 
+@Inherited
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(EnabledOnNativeCondition.class)
