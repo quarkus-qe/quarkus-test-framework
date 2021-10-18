eb979dff4cdf2a0c0706bd697ddfa5447569a6c4
Jose <josecarvajalhilario@gmail.com>
2021-10-18 14:36:37 +0200

    Removed `@EnabledIfKubernetesScenario` and `@EnabledIfOpenShiftScenario`
    These annotations were not used and hence we can remove it.

--- examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingContainerRegistryPingPongResourceIT.java
+++ examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingContainerRegistryPingPongResourceIT.java
@@ -2,9 +2,7 @@ package io.quarkus.qe;
 
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
-import io.quarkus.test.scenarios.annotations.EnabledIfOpenShiftScenarioPropertyIsTrue;
 
-@EnabledIfOpenShiftScenarioPropertyIsTrue
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingContainerRegistry)
 public class OpenShiftUsingContainerRegistryPingPongResourceIT extends PingPongResourceIT {
 }
