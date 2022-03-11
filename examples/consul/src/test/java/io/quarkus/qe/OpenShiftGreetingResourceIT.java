3cfb0cba396952d46117a165354e27fe0d1c225a
pablo gonzalez granados <pagonzal@redhat.com>
2022-03-11 19:12:05 +0100

    Disable consul ocp sceanrio OpenShiftGreetingResourceIT
    quarkus application can't reach consul server, because of a Quarkus DNS resolver issue.

--- examples/consul/src/test/java/io/quarkus/qe/OpenShiftGreetingResourceIT.java
+++ examples/consul/src/test/java/io/quarkus/qe/OpenShiftGreetingResourceIT.java
@@ -1,7 +1,11 @@
 package io.quarkus.qe;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
+@Disabled
+//TODO https://github.com/quarkusio/quarkus/issues/24277
 public class OpenShiftGreetingResourceIT extends GreetingResourceIT {
 }
