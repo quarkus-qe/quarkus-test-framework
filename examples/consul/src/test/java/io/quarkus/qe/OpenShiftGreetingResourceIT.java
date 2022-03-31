efbf0179a68236808e07de5cbb488cb904e66870
Fedor Dudinskiy <fdudinsk@redhat.com>
2022-03-31 15:54:52 +0200

    Use new version of Consul config
    Consul config was relocated from quarkus to quarkiverse
    Bug fixes(including the one for failure on Openshift[1]) are only accessible in quarkusverse artifacts
    
    [1] https://github.com/quarkusio/quarkus/issues/24277

--- examples/consul/src/test/java/io/quarkus/qe/OpenShiftGreetingResourceIT.java
+++ examples/consul/src/test/java/io/quarkus/qe/OpenShiftGreetingResourceIT.java
@@ -1,11 +1,7 @@
 package io.quarkus.qe;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
 @OpenShiftScenario
-@Disabled
-//TODO https://github.com/quarkusio/quarkus/issues/24277
 public class OpenShiftGreetingResourceIT extends GreetingResourceIT {
 }
