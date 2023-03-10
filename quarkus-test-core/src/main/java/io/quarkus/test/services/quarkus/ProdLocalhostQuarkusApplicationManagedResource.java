9de6fad15a781d8aebbd504d190670b856dafcfa
pablo gonzalez granados <pagonzal@redhat.com>
2023-03-10 10:26:34 +0100

    Fix Quarkus args parameters on Quarkus ProdMode

--- quarkus-test-core/src/main/java/io/quarkus/test/services/quarkus/ProdLocalhostQuarkusApplicationManagedResource.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/services/quarkus/ProdLocalhostQuarkusApplicationManagedResource.java
@@ -1,11 +1,16 @@
 package io.quarkus.test.services.quarkus;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 public class ProdLocalhostQuarkusApplicationManagedResource extends LocalhostQuarkusApplicationManagedResource {
 
     private static final String JAVA = "java";
+    private static final String QUARKUS_ARGS_PROPERTY_NAME = "quarkus.args";
 
     private final ProdQuarkusApplicationManagedResourceBuilder model;
 
@@ -16,11 +21,13 @@ public class ProdLocalhostQuarkusApplicationManagedResource extends LocalhostQua
 
     protected List<String> prepareCommand(List<String> systemProperties) {
         List<String> command = new LinkedList<>();
+        String[] cmdArgs = extractQuarkusArgs(systemProperties);
         if (model.getArtifact().getFileName().toString().endsWith(".jar")) {
             command.add(JAVA);
             command.addAll(systemProperties);
             command.add("-jar");
             command.add(model.getArtifact().toAbsolutePath().toString());
+            command.addAll(Arrays.asList(cmdArgs));
         } else {
             command.add(model.getArtifact().toAbsolutePath().toString());
             command.addAll(systemProperties);
@@ -29,4 +36,18 @@ public class ProdLocalhostQuarkusApplicationManagedResource extends LocalhostQua
         return command;
     }
 
+    private String[] extractQuarkusArgs(List<String> systemProperties) {
+        String[] args = ArrayUtils.EMPTY_STRING_ARRAY;
+        Iterator<String> propertiesIt = systemProperties.iterator();
+        while (propertiesIt.hasNext()) {
+            String property = propertiesIt.next();
+            if (property.contains(QUARKUS_ARGS_PROPERTY_NAME)) {
+                propertiesIt.remove();
+                args = property.replace("-D" + QUARKUS_ARGS_PROPERTY_NAME + "=", "").split(" ");
+                break;
+            }
+        }
+
+        return args;
+    }
 }
