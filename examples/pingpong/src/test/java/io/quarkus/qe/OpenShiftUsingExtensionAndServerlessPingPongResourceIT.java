c4fe95391b9df685dd173ea5b357af23aa73cf48
Georgii Troitskii <gtroitsk@redhat.com>
2023-03-24 11:17:04 +0100

    Enable ping-pong resource tests with Quarkus OpenShift extension

--- examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionAndServerlessPingPongResourceIT.java
+++ examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionAndServerlessPingPongResourceIT.java
@@ -1,11 +1,8 @@
 package io.quarkus.qe;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
-@Disabled("https://github.com/quarkusio/quarkus/issues/31787")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
 public class OpenShiftUsingExtensionAndServerlessPingPongResourceIT extends PingPongResourceIT {
 }
