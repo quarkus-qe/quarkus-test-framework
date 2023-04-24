f9d28a4b32418f45ae59ce92d70be2e52a3bc2e7
Georgii Troitskii <gtroitsk@redhat.com>
2023-04-24 15:42:07 +0200

    Enable AMQ tcp tests
    Update quarkus-artemis-jms from 2.1.0 to 3.0.0

--- examples/amq-tcp/src/test/java/io/quarkus/qe/tcp/OpenShiftTcpAmqIT.java
+++ examples/amq-tcp/src/test/java/io/quarkus/qe/tcp/OpenShiftTcpAmqIT.java
@@ -1,11 +1,7 @@
 package io.quarkus.qe.tcp;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
-// TODO: enable with next Quarkus Artemis bump
-@Disabled("Disabled until Quarkus Artemis using Jakarta is released")
 @OpenShiftScenario
 public class OpenShiftTcpAmqIT extends TcpAmqIT {
 
