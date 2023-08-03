9943f4d6948aea7ff4147d3e614e4b733cb7666c
Michal Jurč <mjurc@redhat.com>
2023-08-03 15:13:54 +0200

    Enable tests disabled by quarkus#34276
    * Removing @Disabled annotation from tests disabled by
      https://github.com/quarkusio/quarkus/issues/34276 as the issue has
      been fixed with 3.2.1.Final

--- examples/funqy-knative-events/src/test/java/io/quarkus/qe/funqy/knativeevents/OpenShiftUsingExtensionAndServerlessFunqyKnEventsIT.java
+++ examples/funqy-knative-events/src/test/java/io/quarkus/qe/funqy/knativeevents/OpenShiftUsingExtensionAndServerlessFunqyKnEventsIT.java
@@ -8,13 +8,11 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
-import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusVersion;
 import io.quarkus.test.services.QuarkusApplication;
 import io.quarkus.test.services.knative.eventing.FunqyKnativeEventsService;
 import io.quarkus.test.services.knative.eventing.OpenShiftExtensionFunqyKnativeEventsService;
 
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
-@DisabledOnQuarkusVersion(version = "3.2.0.Final", reason = "https://github.com/quarkusio/quarkus/issues/34276")
 public class OpenShiftUsingExtensionAndServerlessFunqyKnEventsIT {
 
     private static final String INVOKED_FUNCTION = "defaultChain";
