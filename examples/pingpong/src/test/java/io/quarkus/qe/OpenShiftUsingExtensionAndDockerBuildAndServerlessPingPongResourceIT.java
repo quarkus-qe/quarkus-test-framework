360a7ce0f3a39589c04e13da24b2c624698af690
Jakub Jedlicka <jjedlick@redhat.com>
2023-10-04 16:16:01 +0200

    Enable the disable test affected by quarkusio/quarkus#35288

--- examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionAndDockerBuildAndServerlessPingPongResourceIT.java
+++ examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionAndDockerBuildAndServerlessPingPongResourceIT.java
@@ -1,11 +1,8 @@
 package io.quarkus.qe;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
-@Disabled("https://github.com/quarkusio/quarkus/issues/35288")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtensionAndDockerBuildStrategy)
 public class OpenShiftUsingExtensionAndDockerBuildAndServerlessPingPongResourceIT extends PingPongResourceIT {
 }
