bfa593e845de81cce863232fdeed9a09e4be25ba
Michal Jurč <mjurc@redhat.com>
2023-10-15 00:33:44 +0200

    Enable Camel example external applications
    * https://github.com/apache/camel-quarkus-examples.git is now on Quarkus 3.y,
      so we should be fine re-enabling them.

--- examples/external-applications/src/test/java/io/quarkus/qe/OpenShiftExtensionCamelFileBindyFtpIT.java
+++ examples/external-applications/src/test/java/io/quarkus/qe/OpenShiftExtensionCamelFileBindyFtpIT.java
@@ -7,11 +7,8 @@ import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusSnapshot;
-import io.quarkus.test.scenarios.annotations.DisabledOnQuarkusVersion;
 import io.quarkus.test.services.GitRepositoryQuarkusApplication;
 
-// TODO: enable test when Camel Quarkus Examples migrate to Quarkus 3.0
-@DisabledOnQuarkusVersion(version = "(3\\.[0-9]\\..*)", reason = "Camel Quarkus Examples is using Quarkus 2.16")
 @DisabledOnQuarkusSnapshot(reason = "Camel Quarkus 999-SNAPSHOT is not available in maven repository") // f.e. 'quarkus-camel-bom:pom:999-SNAPSHOT' is not available
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
 public class OpenShiftExtensionCamelFileBindyFtpIT {
