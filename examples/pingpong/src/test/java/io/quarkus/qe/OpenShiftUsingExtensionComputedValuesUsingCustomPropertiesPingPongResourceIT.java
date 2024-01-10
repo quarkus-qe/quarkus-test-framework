389d591485e36989cf0f5aeb7167eb91b94eece9
Martin Ocenas <mocenas@redhat.com>
2024-01-10 11:36:25 +0100

    order imports

--- examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionComputedValuesUsingCustomPropertiesPingPongResourceIT.java
+++ examples/pingpong/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionComputedValuesUsingCustomPropertiesPingPongResourceIT.java
@@ -1,8 +1,9 @@
 package io.quarkus.qe;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
-import org.junit.jupiter.api.Disabled;
 
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
 @Disabled("https://github.com/quarkusio/quarkus/issues/38018")
