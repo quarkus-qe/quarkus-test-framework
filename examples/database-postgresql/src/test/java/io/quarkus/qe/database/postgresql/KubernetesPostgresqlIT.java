fa8c848f5f64c5b4770a8039b6950061cfd987c1
Georgii Troitskii <gtroitsk@redhat.com>
2024-03-10 17:39:27 +0100

    Add Kubernetes test for PostgreSQL

---
+++ examples/database-postgresql/src/test/java/io/quarkus/qe/database/postgresql/KubernetesPostgresqlIT.java
@@ -0,0 +1,28 @@
+package io.quarkus.qe.database.postgresql;
+
+import io.quarkus.test.bootstrap.PostgresqlService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.KubernetesScenario;
+import io.quarkus.test.services.Container;
+import io.quarkus.test.services.QuarkusApplication;
+
+@KubernetesScenario
+public class KubernetesPostgresqlIT extends AbstractSqlDatabaseIT {
+
+    private static final int POSTGRESQL_PORT = 5432;
+
+    @Container(image = "${postgresql.image}", port = POSTGRESQL_PORT, expectedLog = "is ready")
+    static PostgresqlService database = new PostgresqlService()
+            .withProperty("PGDATA", "/tmp/psql");
+
+    @QuarkusApplication
+    static RestService app = new RestService()
+            .withProperty("quarkus.datasource.username", database.getUser())
+            .withProperty("quarkus.datasource.password", database.getPassword())
+            .withProperty("quarkus.datasource.jdbc.url", database::getJdbcUrl);
+
+    @Override
+    protected RestService getApp() {
+        return app;
+    }
+}
