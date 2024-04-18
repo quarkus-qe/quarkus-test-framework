fed3d789aca9f476e7e82856a6ffca0ad66ff7a1
Michal Vavřík <mvavrik@redhat.com>
2024-04-18 22:03:48 +0200

    Fix Kafka SSL scenarios on FIPS by regenerating certs

--- examples/kafka/src/test/java/io/quarkus/qe/StrimziKafkaWithDefaultSaslSslMessagingIT.java
+++ examples/kafka/src/test/java/io/quarkus/qe/StrimziKafkaWithDefaultSaslSslMessagingIT.java
@@ -17,24 +17,13 @@ import io.quarkus.test.services.containers.model.KafkaVendor;
 @QuarkusScenario
 public class StrimziKafkaWithDefaultSaslSslMessagingIT {
 
-    private final static String SASL_USERNAME_VALUE = "client";
-    private final static String SASL_PASSWORD_VALUE = "client-secret";
-    private static final String TRUSTSTORE_FILE = "strimzi-server-ssl-truststore.p12";
-
-    @KafkaContainer(vendor = KafkaVendor.STRIMZI, protocol = KafkaProtocol.SASL_SSL, kafkaConfigResources = TRUSTSTORE_FILE)
+    @KafkaContainer(vendor = KafkaVendor.STRIMZI, protocol = KafkaProtocol.SASL_SSL)
     static final KafkaService kafka = new KafkaService();
 
     @QuarkusApplication
     static final RestService app = new RestService()
-            .withProperty("kafka.bootstrap.servers", kafka::getBootstrapUrl)
-            .withProperty("kafka.security.protocol", "SASL_SSL")
-            .withProperty("kafka.ssl.truststore.location", TRUSTSTORE_FILE)
-            .withProperty("kafka.ssl.truststore.password", "top-secret")
-            .withProperty("kafka.ssl.truststore.type", "PKCS12")
-            .withProperty("kafka.sasl.mechanism", "PLAIN")
-            .withProperty("kafka.sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required "
-                    + "username=\"" + SASL_USERNAME_VALUE + "\" "
-                    + "password=\"" + SASL_PASSWORD_VALUE + "\";");
+            .withProperties(kafka::getSslProperties)
+            .withProperty("kafka.bootstrap.servers", kafka::getBootstrapUrl);
 
     @Test
     public void checkUserResourceByNormalUser() {
