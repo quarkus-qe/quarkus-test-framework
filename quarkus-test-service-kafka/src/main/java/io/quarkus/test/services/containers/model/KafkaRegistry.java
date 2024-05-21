74fe6a1640b143aab62d4cbd70926de2197f157b
Jakub Jedlicka <jjedlick@redhat.com>
2024-05-21 19:42:13 +0200

    Bump strimzi kafka to version 0.41.0-kafka-3.7.0 and apicurio to 2.5.11.Final

--- quarkus-test-service-kafka/src/main/java/io/quarkus/test/services/containers/model/KafkaRegistry.java
+++ quarkus-test-service-kafka/src/main/java/io/quarkus/test/services/containers/model/KafkaRegistry.java
@@ -2,7 +2,7 @@ package io.quarkus.test.services.containers.model;
 
 public enum KafkaRegistry {
     CONFLUENT("confluentinc/cp-schema-registry", "7.5.3", "/", 8081),
-    APICURIO("quay.io/apicurio/apicurio-registry-mem", "2.5.8.Final", "/apis", 8080);
+    APICURIO("quay.io/apicurio/apicurio-registry-mem", "2.5.11.Final", "/apis", 8080);
 
     private final String image;
     private final String defaultVersion;
