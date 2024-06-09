ef853dd0832d1a80eac9184b5869af9960a859a6
Jakub Jedlicka <jjedlick@redhat.com>
2024-06-10 01:19:19 +0200

    Pingpong and consul example change from resteasy to rest

--- examples/consul/src/main/java/io/quarkus/qe/GreetingResource.java
+++ examples/consul/src/main/java/io/quarkus/qe/GreetingResource.java
@@ -7,6 +7,8 @@ import jakarta.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.smallrye.mutiny.Uni;
+
 @Path("/api")
 public class GreetingResource {
 
@@ -18,8 +20,8 @@ public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello " + property;
+    public Uni<String> hello() {
+        return Uni.createFrom().item("Hello " + property);
     }
 
     @GET
