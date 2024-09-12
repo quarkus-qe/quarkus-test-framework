379ca825ed0dee052380ab69a3aabf153841dace
Michal Vavřík <mvavrik@redhat.com>
2024-09-12 21:04:05 +0200

    Fix PostgreSQL waiting strategy for RH image
    (cherry picked from commit 1fd76acc9c14ee454d3d513a9969d168edb2de4d)

--- quarkus-test-service-database/src/main/java/io/quarkus/test/bootstrap/PostgresqlService.java
+++ quarkus-test-service-database/src/main/java/io/quarkus/test/bootstrap/PostgresqlService.java
@@ -2,6 +2,13 @@ package io.quarkus.test.bootstrap;
 
 public class PostgresqlService extends DatabaseService<PostgresqlService> {
 
+    /**
+     * Red Hat PostgreSQL container image at certain point redirects logs to a file.
+     * This is name of property that determines destination.
+     * More info here:
+     * https://github.com/sclorg/postgresql-container/blob/master/16/root/usr/share/container-scripts/postgresql/README.md
+     */
+    static final String LOG_DESTINATION = "POSTGRESQL_LOG_DESTINATION";
     static final String USER_PROPERTY = "POSTGRESQL_USER";
     static final String PASSWORD_PROPERTY = "POSTGRESQL_PASSWORD";
     static final String DATABASE_PROPERTY = "POSTGRESQL_DATABASE";
@@ -23,6 +30,7 @@ public class PostgresqlService extends DatabaseService<PostgresqlService> {
         withProperty(USER_PROPERTY, getUser());
         withProperty(PASSWORD_PROPERTY, getPassword());
         withProperty(DATABASE_PROPERTY, getDatabase());
+        withProperty(LOG_DESTINATION, "/dev/stdout");
 
         // DockerHub environment variables
         withProperty(DH_USER_PROPERTY, getUser());
