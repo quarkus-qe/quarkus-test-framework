d0591ac904f91c882810dfe24204f6360c922a70
jcarranzan <jcarranz@redhat.com>
2024-09-24 10:57:22 +0200

    Remove inheritance in QuickstarsUsingDefaultsIT to isolate test logic and prevent shared resources conflicts on ocp
    (cherry picked from commit eb127f0492c8bb424ca59c3fea1e1aa77ddd767a)

--- examples/external-applications/src/test/java/io/quarkus/qe/OpenShiftContainerRegistryQuickstartUsingDefaultsIT.java
+++ examples/external-applications/src/test/java/io/quarkus/qe/OpenShiftContainerRegistryQuickstartUsingDefaultsIT.java
@@ -1,9 +1,17 @@
 package io.quarkus.qe;
 
+import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
+import io.quarkus.test.services.GitRepositoryQuarkusApplication;
 
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingContainerRegistry)
 public class OpenShiftContainerRegistryQuickstartUsingDefaultsIT extends QuickstartUsingDefaultsIT {
+    @GitRepositoryQuarkusApplication(repo = "https://github.com/quarkusio/quarkus-quickstarts.git", contextDir = "getting-started")
+    static final RestService app = new RestService();
 
+    @Override
+    protected RestService getApp() {
+        return app;
+    }
 }
