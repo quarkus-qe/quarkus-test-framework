f5d7eeb3a4a107354e7920015e787c5ea67c6809
Michal Vavřík <mvavrik@redhat.com>
2024-10-23 11:57:25 +0200

    Add @DisabledOnFipsAndNative & drop version with Java 17
    (cherry picked from commit 528f2052da1edd68fa2a9ee0aa2bbceac4852ea9)

--- quarkus-test-core/src/main/java/io/quarkus/test/scenarios/annotations/DisabledOnFipsAndNativeCondition.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/scenarios/annotations/DisabledOnFipsAndNativeCondition.java
@@ -1,31 +1,29 @@
 package io.quarkus.test.scenarios.annotations;
 
+import static io.quarkus.test.services.quarkus.model.QuarkusProperties.isNativeEnabled;
+
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
 import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class DisabledOnFipsAndJava17Condition implements ExecutionCondition {
+public class DisabledOnFipsAndNativeCondition implements ExecutionCondition {
 
     /**
      * We set environment variable "FIPS" to "fips" in our Jenkins jobs when FIPS are enabled.
      */
     private static final String FIPS_ENABLED = "fips";
-    private static final int JAVA_17 = 17;
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
-        if (isFipsEnabledEnvironment() && isJava17()) {
-            return ConditionEvaluationResult.disabled("The test is running in FIPS enabled environment with Java 17");
+        if (isFipsEnabledEnvironment() && isNativeEnabled()) {
+            return ConditionEvaluationResult.disabled("The test is running in FIPS enabled environment in native mode");
         }
 
-        return ConditionEvaluationResult.enabled("The test is not running in FIPS enabled environment with Java 17");
+        return ConditionEvaluationResult.enabled("The test is not running in FIPS enabled environment in native mode");
     }
 
     private static boolean isFipsEnabledEnvironment() {
-        return FIPS_ENABLED.equals(System.getenv().get("FIPS"));
+        return FIPS_ENABLED.equalsIgnoreCase(System.getenv().get("FIPS"));
     }
 
-    private static boolean isJava17() {
-        return JAVA_17 == Runtime.version().feature();
-    }
 }
