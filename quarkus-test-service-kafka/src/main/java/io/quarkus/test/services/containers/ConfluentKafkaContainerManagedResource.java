3461af5a106d1391bd8c47624c876119d1256e6c
Fedor Dudinsky <fdudinsk@redhat.com>
2024-11-21 13:12:53 +0100

    Do not use deprecated class
    Change in Kafka containers also led in some config changes,
    since testcontainers switched to KRaft under the hood,and changed ports
    9093 was used for plaintext and 9092 for broker,
    now it is 9092 for plaintext and 9093 for broker

--- quarkus-test-service-kafka/src/main/java/io/quarkus/test/services/containers/ConfluentKafkaContainerManagedResource.java
+++ quarkus-test-service-kafka/src/main/java/io/quarkus/test/services/containers/ConfluentKafkaContainerManagedResource.java
@@ -2,7 +2,7 @@ package io.quarkus.test.services.containers;
 
 import org.apache.commons.lang3.StringUtils;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import io.quarkus.test.utils.DockerUtils;
@@ -20,7 +20,7 @@ public class ConfluentKafkaContainerManagedResource extends BaseKafkaContainerMa
 
     @Override
     protected GenericContainer<?> initKafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse(getKafkaImage() + ":" + getKafkaVersion()))
+        return new ConfluentKafkaContainer(DockerImageName.parse(getKafkaImage() + ":" + getKafkaVersion()))
                 .withCreateContainerCmdModifier(cmd -> cmd.withName(DockerUtils.generateDockerContainerName()));
     }
 
@@ -31,7 +31,7 @@ public class ConfluentKafkaContainerManagedResource extends BaseKafkaContainerMa
         schemaRegistry.withEnv("SCHEMA_REGISTRY_HOST_NAME", "schema-registry");
         schemaRegistry.withEnv("SCHEMA_REGISTRY_LISTENERS", "http://0.0.0.0:" + getKafkaRegistryPort());
         schemaRegistry.withEnv("SCHEMA_REGISTRY_KAFKASTORE_BOOTSTRAP_SERVERS",
-                "PLAINTEXT://" + kafka.getNetworkAliases().get(0) + ":9092");
+                "PLAINTEXT://" + kafka.getNetworkAliases().get(0) + ":9093");
         schemaRegistry.withCreateContainerCmdModifier(cmd -> cmd.withName(DockerUtils.generateDockerContainerName()));
 
         return schemaRegistry;
