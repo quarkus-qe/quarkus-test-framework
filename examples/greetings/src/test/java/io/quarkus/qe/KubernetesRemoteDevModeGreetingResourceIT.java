de0f0ca915bb2963e314788f3feb5dffaa5dddb2
Troitskii Georgii <g.troitskii@gmail.com>
2025-06-17 11:26:33 +0200

    Disable KubernetesRemoteDevModeGreetingResourceIT due to upstream issue
    https://github.com/quarkusio/quarkus/issues/48198

--- examples/greetings/src/test/java/io/quarkus/qe/KubernetesRemoteDevModeGreetingResourceIT.java
+++ examples/greetings/src/test/java/io/quarkus/qe/KubernetesRemoteDevModeGreetingResourceIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.qe;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.KubernetesScenario;
 
+@Disabled("https://github.com/quarkusio/quarkus/issues/48198")
 @KubernetesScenario
 public class KubernetesRemoteDevModeGreetingResourceIT extends RemoteDevModeGreetingResourceIT {
 }
