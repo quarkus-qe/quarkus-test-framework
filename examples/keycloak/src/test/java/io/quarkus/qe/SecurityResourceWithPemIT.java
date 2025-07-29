f77c9e739b7902d9da65fcc14641f07ddc633483
Jakub Jedlicka <jjedlick@redhat.com>
2025-07-29 23:54:21 +0200

    Modify and add tests to run Keycloak in production mode

--- examples/keycloak/src/test/java/io/quarkus/qe/SecurityResourceWithPemIT.java
+++ examples/keycloak/src/test/java/io/quarkus/qe/SecurityResourceWithPemIT.java
@@ -7,16 +7,17 @@ import static io.quarkus.test.bootstrap.KeycloakService.DEFAULT_REALM_FILE;
 import io.quarkus.test.bootstrap.KeycloakService;
 import io.quarkus.test.bootstrap.RestService;
 import io.quarkus.test.scenarios.QuarkusScenario;
-import io.quarkus.test.services.DevModeQuarkusApplication;
+import io.quarkus.test.services.Certificate;
 import io.quarkus.test.services.KeycloakContainer;
+import io.quarkus.test.services.QuarkusApplication;
 
 @QuarkusScenario
-public class DevModeSecurityResourceIT extends BaseSecurityResourceIT {
+public class SecurityResourceWithPemIT extends BaseSecurityResourceIT {
 
-    @KeycloakContainer(runKeycloakInProdMode = true)
+    @KeycloakContainer(runKeycloakInProdMode = true, certificateFormat = Certificate.Format.PEM)
     static final KeycloakService keycloak = new KeycloakService(DEFAULT_REALM_FILE, DEFAULT_REALM, DEFAULT_REALM_BASE_PATH);
 
-    @DevModeQuarkusApplication
+    @QuarkusApplication
     static final RestService app = new RestService()
             .withProperty("quarkus.oidc.auth-server-url", keycloak::getRealmUrl)
             .withProperty("quarkus.oidc.client-id", CLIENT_ID_DEFAULT)
