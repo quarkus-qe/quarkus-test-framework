42f16f7db0a217f082d1a1d9e6a1afa063bbbf5c
Michal Jurc <mjurc@redhat.com>
2025-08-25 20:34:14 +0200

    Re-enable KubernetesRemoteDevModeGreetingResourceIT as the root cause has been fixed
    * See https://github.com/quarkusio/quarkus/issues/48198

--- examples/greetings/src/test/java/io/quarkus/qe/KubernetesRemoteDevModeGreetingResourceIT.java
+++ examples/greetings/src/test/java/io/quarkus/qe/KubernetesRemoteDevModeGreetingResourceIT.java
@@ -1,10 +1,7 @@
 package io.quarkus.qe;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.KubernetesScenario;
 
-@Disabled("https://github.com/quarkusio/quarkus/issues/48198")
 @KubernetesScenario
 public class KubernetesRemoteDevModeGreetingResourceIT extends RemoteDevModeGreetingResourceIT {
 }
