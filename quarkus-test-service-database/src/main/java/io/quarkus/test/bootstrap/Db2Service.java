79965f89fdf36d50836bd21768a6238d68384976
Michal Vavřík <michal.vavrik@aol.com>
2025-09-02 14:03:07 +0200

    Dont use path in configmaps and secrets
    (cherry picked from commit c5d1e0ad60b27d75f38804501e7675cacb8510ce)

--- quarkus-test-service-database/src/main/java/io/quarkus/test/bootstrap/Db2Service.java
+++ quarkus-test-service-database/src/main/java/io/quarkus/test/bootstrap/Db2Service.java
@@ -7,20 +7,19 @@ public class Db2Service extends DatabaseService<Db2Service> {
     static final String DATABASE_PROPERTY = "DBNAME";
     static final String JDBC_NAME = "db2";
 
+    public Db2Service() {
+        withProperty("AUTOCONFIG", "false");
+        withProperty("ARCHIVE_LOGS", "false");
+        withProperty("LICENSE", "accept");
+        onPreStart(service -> service
+                .withProperty(USER_PROPERTY, getUser())
+                .withProperty(PASSWORD_PROPERTY, getPassword())
+                .withProperty(DATABASE_PROPERTY, getDatabase()));
+    }
+
     @Override
     protected String getJdbcName() {
         return JDBC_NAME;
     }
 
-    @Override
-    public Db2Service onPreStart(Action action) {
-        withProperty(USER_PROPERTY, getUser());
-        withProperty(PASSWORD_PROPERTY, getPassword());
-        withProperty(DATABASE_PROPERTY, getDatabase());
-        withProperty("AUTOCONFIG", "false");
-        withProperty("ARCHIVE_LOGS", "false");
-        withProperty("LICENSE", "accept");
-
-        return super.onPreStart(action);
-    }
 }
--- quarkus-test-service-database/src/main/java/io/quarkus/test/bootstrap/MySqlService.java
+++ quarkus-test-service-database/src/main/java/io/quarkus/test/bootstrap/MySqlService.java
@@ -8,18 +8,17 @@ public class MySqlService extends DatabaseService<MySqlService> {
     static final String DATABASE_PROPERTY = "MYSQL_DATABASE";
     static final String JDBC_NAME = "mysql";
 
+    public MySqlService() {
+        onPreStart(service -> service
+                .withProperty(USER_PROPERTY, getUser())
+                .withProperty(PASSWORD_PROPERTY, getPassword())
+                .withProperty(PASSWORD_ROOT_PROPERTY, getPassword())
+                .withProperty(DATABASE_PROPERTY, getDatabase()));
+    }
+
     @Override
     protected String getJdbcName() {
         return JDBC_NAME;
     }
 
-    @Override
-    public MySqlService onPreStart(Action action) {
-        withProperty(USER_PROPERTY, getUser());
-        withProperty(PASSWORD_PROPERTY, getPassword());
-        withProperty(PASSWORD_ROOT_PROPERTY, getPassword());
-        withProperty(DATABASE_PROPERTY, getDatabase());
-
-        return super.onPreStart(action);
-    }
 }
