f86878b5b79de876f8d70703005e68f4945ec5c2
Michal Vavřík <michal.vavrik@aol.com>
2025-09-26 20:14:01 +0200

    Use HashiCorp Consul image

--- examples/consul/src/test/java/io/quarkus/qe/OpenShiftGreetingResourceIT.java
+++ examples/consul/src/test/java/io/quarkus/qe/OpenShiftGreetingResourceIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.qe;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
+@Disabled("https://github.com/quarkus-qe/quarkus-test-framework/issues/1708")
 @OpenShiftScenario
 public class OpenShiftGreetingResourceIT extends GreetingResourceIT {
 }
