fdd3f7a248f278d04a4b706c5094c9e608f46c81
Jakub Jedlicka <jjedlick@redhat.com>
2025-09-30 19:22:39 +0200

    Enable consul on openshift, by using official openshift hashicorp/consul image

--- examples/consul/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionBuildTimeGreetingResourceIT.java
+++ examples/consul/src/test/java/io/quarkus/qe/OpenShiftUsingExtensionBuildTimeGreetingResourceIT.java
@@ -1,11 +1,8 @@
 package io.quarkus.qe;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.scenarios.OpenShiftDeploymentStrategy;
 import io.quarkus.test.scenarios.OpenShiftScenario;
 
-@Disabled("https://github.com/quarkus-qe/quarkus-test-framework/issues/1708")
 @OpenShiftScenario(deployment = OpenShiftDeploymentStrategy.UsingOpenShiftExtension)
 public class OpenShiftUsingExtensionBuildTimeGreetingResourceIT extends BuildTimeGreetingResourceIT {
 }
