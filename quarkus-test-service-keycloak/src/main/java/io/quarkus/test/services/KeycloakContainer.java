79fa337c84a1466115078e625e0d0496cfb35bd3
Jakub Jedlicka <jjedlick@redhat.com>
2025-10-30 18:11:49 +0100

    Bump Keycloak to 26.4

--- quarkus-test-service-keycloak/src/main/java/io/quarkus/test/services/KeycloakContainer.java
+++ quarkus-test-service-keycloak/src/main/java/io/quarkus/test/services/KeycloakContainer.java
@@ -14,7 +14,7 @@ import io.quarkus.test.services.containers.KeycloakContainerManagedResourceBuild
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface KeycloakContainer {
-    String image() default "quay.io/keycloak/keycloak:26.3";
+    String image() default "quay.io/keycloak/keycloak:26.4";
 
     int port() default 8080;
 
