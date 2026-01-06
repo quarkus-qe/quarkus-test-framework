858603c6c54903b49b1a032a39222b234bc6b928
Jakub Jedlicka <jjedlick@redhat.com>
2026-01-06 22:07:33 +0100

    Add option to pass the JVM option arguments/properties
    (cherry picked from commit 1fedbc749c637cf5478aefdbe7f0d47c0c904b84)

--- quarkus-test-core/src/main/java/io/quarkus/test/utils/PropertiesUtils.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/utils/PropertiesUtils.java
@@ -108,4 +108,9 @@ public final class PropertiesUtils {
                 + "=" + propertyValue;
     }
 
+    public static String toJvmSystemProperty(String propertyKey, String propertyValue) {
+        return (OS.WINDOWS.isCurrentOs() ? propertyKey.replace("\"", "\\\"") : propertyKey)
+                + "=" + propertyValue;
+    }
+
 }
