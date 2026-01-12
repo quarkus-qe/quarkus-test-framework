23859dbef5940adc18c3063b8e686108c7de95a6
Michal Vavřík <michal.vavrik@aol.com>
2026-01-12 13:52:12 +0100

    Fix flaky RemoteDevModeGreetingResourceIT
    Signed-off-by: Michal Vavřík <michal.vavrik@aol.com>
    (cherry picked from commit 6546b60cb5cd01e9c6b8ab7462374b3ab5424cee)

--- examples/greetings/src/test/java/io/quarkus/qe/RemoteDevModeGreetingResourceIT.java
+++ examples/greetings/src/test/java/io/quarkus/qe/RemoteDevModeGreetingResourceIT.java
@@ -1,11 +1,14 @@
 package io.quarkus.qe;
 
+import static io.quarkus.test.services.quarkus.RemoteDevModeQuarkusApplicationManagedResourceBuilder.EXPECTED_OUTPUT_FROM_REMOTE_DEV_DAEMON;
 import static org.hamcrest.Matchers.is;
 
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.bootstrap.DevModeQuarkusService;
+import io.quarkus.test.bootstrap.Protocol;
+import io.quarkus.test.logging.Log;
 import io.quarkus.test.scenarios.QuarkusScenario;
 import io.quarkus.test.services.IsRunningCheck;
 import io.quarkus.test.services.RemoteDevModeQuarkusApplication;
@@ -19,7 +22,9 @@ public class RemoteDevModeGreetingResourceIT {
     static final String HELLO_IN_ENGLISH = "Hello";
     static final String HELLO_IN_SPANISH = "Hola";
 
-    @RemoteDevModeQuarkusApplication(isRunningCheck = IsRunningCheck.IsBasePathReachableCheck.class)
+    static final String EXPECTED_OUTPUT_REMOTE_DEV_REQ_FAILED = "Remote dev request failed";
+
+    @RemoteDevModeQuarkusApplication(isRunningCheck = IsGreetingPathReachableCheck.class)
     static final DevModeQuarkusService app = new DevModeQuarkusService();
 
     @Test
@@ -40,5 +45,33 @@ public class RemoteDevModeGreetingResourceIT {
     @Test
     public void shouldLoadResources() {
         app.given().get("/greeting/file").then().statusCode(HttpStatus.SC_OK).body(is("found!"));
+    }
+
+    public static final class IsGreetingPathReachableCheck extends IsRunningCheck.IsPathReachableCheck {
+
+        public IsGreetingPathReachableCheck() {
+            super(Protocol.HTTP, "/greeting", VICTOR_NAME);
+        }
+
+        @Override
+        public boolean isRunning(IsRunningCheckContext context) {
+            if (super.isRunning(context)) {
+                // io.quarkus.test.services.quarkus.RemoteDevModeLocalhostQuarkusApplicationManagedResource.isRunning
+                // does this by default, it is the right thing to do
+                if (app.getLogs().stream().anyMatch(l -> l.contains(EXPECTED_OUTPUT_FROM_REMOTE_DEV_DAEMON))) {
+                    Log.debug("Logs contain expected output '%s'", EXPECTED_OUTPUT_FROM_REMOTE_DEV_DAEMON);
+                    return true;
+                }
+                // but if it didn't work, there are still legit situations when this output is not logged, but
+                // Quarkus can still recover: https://github.com/quarkusio/quarkus/issues/48198#issuecomment-2939698255
+                // so here, we will just trust the IsPathReachableCheck result
+                if (app.getLogs().stream().anyMatch(l -> l.contains(EXPECTED_OUTPUT_REMOTE_DEV_REQ_FAILED))) {
+                    Log.debug("Logs contain expected output '%s'", EXPECTED_OUTPUT_REMOTE_DEV_REQ_FAILED);
+                    return true;
+                }
+            }
+            return false;
+
+        }
     }
 }
