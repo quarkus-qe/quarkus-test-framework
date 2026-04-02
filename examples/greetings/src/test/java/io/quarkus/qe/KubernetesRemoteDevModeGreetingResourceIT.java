35ca39212e67d6a5553e0e59a0db0d363b728ed0
Michal Jurc <mjurc@redhat.com>
2026-04-02 02:25:02 +0200

    Disabling KubernetesRemoteDevModeGreetingResourceIT due to #53407
    * https://github.com/quarkusio/quarkus/issues/53407

--- examples/greetings/src/test/java/io/quarkus/qe/KubernetesRemoteDevModeGreetingResourceIT.java
+++ examples/greetings/src/test/java/io/quarkus/qe/KubernetesRemoteDevModeGreetingResourceIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.qe;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.scenarios.KubernetesScenario;
 
 @KubernetesScenario
+@Disabled("https://github.com/quarkusio/quarkus/issues/53407")
 public class KubernetesRemoteDevModeGreetingResourceIT extends RemoteDevModeGreetingResourceIT {
 }
