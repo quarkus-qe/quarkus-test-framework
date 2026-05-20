c31bc377d201fb725f88212598cf2cfe564edfcd
Jakub Jedlicka <jjedlick@redhat.com>
2026-05-20 21:20:37 +0200

    Add initialize-at-run-time for native and remove disable test

--- quarkus-test-core/src/main/java/io/quarkus/test/scenarios/annotations/DisabledOnFipsAndNativeCondition.java
+++ quarkus-test-core/src/main/java/io/quarkus/test/scenarios/annotations/DisabledOnFipsAndNativeCondition.java
@@ -1,17 +1,13 @@
 package io.quarkus.test.scenarios.annotations;
 
 import static io.quarkus.test.services.quarkus.model.QuarkusProperties.isNativeEnabled;
+import static io.quarkus.test.utils.FipsUtils.isFipsEnabledEnvironment;
 
 import org.junit.jupiter.api.extension.ConditionEvaluationResult;
 import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class DisabledOnFipsAndNativeCondition implements ExecutionCondition {
-
-    /**
-     * We set environment variable "FIPS" to "fips" in our Jenkins jobs when FIPS are enabled.
-     */
-    private static final String FIPS_ENABLED = "fips";
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
@@ -20,10 +16,6 @@ public class DisabledOnFipsAndNativeCondition implements ExecutionCondition {
         }
 
         return ConditionEvaluationResult.enabled("The test is not running in FIPS enabled environment in native mode");
-    }
-
-    private static boolean isFipsEnabledEnvironment() {
-        return FIPS_ENABLED.equalsIgnoreCase(System.getenv().get("FIPS"));
     }
 
 }
