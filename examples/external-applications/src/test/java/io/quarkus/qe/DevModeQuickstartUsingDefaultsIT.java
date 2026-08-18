374ad6ca0e350251a0541c90c6f0831cb1c14562
Jakub Jedlicka <jjedlick@redhat.com>
2026-08-19 00:41:21 +0200

    Prepare FW for 1.9.z branch

--- examples/external-applications/src/test/java/io/quarkus/qe/DevModeQuickstartUsingDefaultsIT.java
+++ examples/external-applications/src/test/java/io/quarkus/qe/DevModeQuickstartUsingDefaultsIT.java
@@ -14,7 +14,7 @@ import io.quarkus.test.services.GitRepositoryQuarkusApplication;
 public class DevModeQuickstartUsingDefaultsIT {
 
     @GitRepositoryQuarkusApplication(repo = "https://github.com/quarkusio/quarkus-quickstarts.git",
-            contextDir = "getting-started", devMode = true, branch = "3.x")
+            contextDir = "getting-started", devMode = true, branch = "3.39")
     static final RestService app = new RestService();
 
     @Test
